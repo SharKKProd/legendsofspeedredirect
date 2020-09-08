c3722d769fd18f063ccb8c6a4606e0909dc9982d
SharKKProd <66116801+SharKKProd@users.noreply.github.com>
2020-09-08 14:45:49 +0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/SharKKProd/legendsofspeedscript/master/rebuild_v2_1.lua'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/SharKKProd/legendsofspeedscript/master/cheat2-1rebuildlast.lua'),true))()
