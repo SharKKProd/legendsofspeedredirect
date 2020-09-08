eabfe250376d6667a3af853282183f9818019008
SharKKProd <66116801+SharKKProd@users.noreply.github.com>
2020-09-08 14:27:58 +0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/SharKKProd/legendsofspeedscript/master/v2cheat.lua'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/SharKKProd/legendsofspeedscript/master/v2_1_lof.lua'),true))()
